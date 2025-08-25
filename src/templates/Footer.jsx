d7c07d9b84efa0b396be37a0ce1fa46ab01fccc2
rafael-cena <rafaelcena4@gmail.com>
2025-08-25 01:15:04 -0300

    final commit v1.1

--- src/templates/Footer.jsx
+++ src/templates/Footer.jsx
@@ -6,16 +6,38 @@ import logus_claro from '../assets/logus-claro.png';
 import logus_escuro from '../assets/logus-escuro.png';
 
 export default function Footer() {
-    const { theme, options } = useContext(Contexto);
+    const { theme, setTheme, options } = useContext(Contexto);
 
     return <footer className="footer" style={options ? { pointerEvents: 'none', filter: 'blur(4px)' } : { pointerEvents: 'all' }}>
-        <div className="footer-img">
-            <img src={theme === 'escuro' ? logus_escuro : logus_claro} alt="logus" />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <h4 className="footer-subtitle">
+                Selecione o tema
+            </h4>
+            <div className="footer-theme">
+                <button
+                    onClick={() => setTheme('claro')}
+                    className={`btn-theme${theme === 'claro' ? ' active' : ''}`}
+                >
+                    claro
+                </button>
+                <button
+                    onClick={() => setTheme('escuro')}
+                    className={`btn-theme${theme === 'escuro' ? ' active' : ''}`}
+                >
+                    escuro
+                </button>
+            </div>
         </div>
-        <div className="footer-link">
-            <Link to={'/'}>
-                logus.fun
-            </Link>
+
+        <div className="footer-g2">
+            <div className="footer-img">
+                <img src={theme === 'escuro' ? logus_escuro : logus_claro} alt="logus" />
+            </div>
+            <div className="footer-link">
+                <Link to={'/'}>
+                    logus.fun
+                </Link>
+            </div>
         </div>
     </footer>
 }
--- src/templates/Navbar.jsx
+++ src/templates/Navbar.jsx
@@ -1,13 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Contexto } from '../App';
 import logus_claro from '../assets/logus-claro.png';
 import logus_escuro from '../assets/logus-escuro.png';
 
 export default function Navbar() {
     const { theme, setOptions } = useContext(Contexto);
-    
+    const [show, setShow] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const controlNavbar = () => {
+            if (window.scrollY > lastScrollY) {
+                setShow(false);
+            } else {
+                setShow(true);
+            }
+            setLastScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', controlNavbar);
+        return () => {
+            window.removeEventListener('scroll', controlNavbar);
+        };
+    }, [lastScrollY]);
+
     return <nav>
-        <section className="navbar">
+        <section className={`navbar ${show ? 'show' : 'hide'}`}>
             <img onClick={() => setOptions(true)} src={theme === 'escuro' ? logus_escuro : logus_claro} alt="logus" />
             logus
         </section>
